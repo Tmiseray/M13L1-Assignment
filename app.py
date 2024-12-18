c2e7aa086700997dd39ba16f645beff1e899a83a
Tmiseray <161903165+Tmiseray@users.noreply.github.com>
2024-12-17 19:55:44 -0600

    Committing test data before running additional tests

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
     configure_rate_limit()
 
     with app.app_context():
-        db.drop_all()
+        # db.drop_all()
         db.create_all()
 
     app.run(debug=True)
