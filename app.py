098d8296e13541b7d1e0ce09daab37dd9f70121e
Tmiseray <161903165+Tmiseray@users.noreply.github.com>
2024-12-17 19:41:57 -0600

    Fixing typos

--- app.py
+++ app.py
@@ -9,12 +9,14 @@ from models.customer import Customer
 from models.product import Product
 from models.order import Order
 from models.production import Production
+from models.user import User
 
 from routes.employeeBP import employee_blueprint
 from routes.customerBP import customer_blueprint
 from routes.productBP import product_blueprint
 from routes.orderBP import order_blueprint
 from routes.productionBP import production_blueprint
+from routes.userBP import user_blueprint
 
 
 def create_app(config_name):
@@ -35,6 +37,7 @@ def blueprint_config(app):
     app.register_blueprint(product_blueprint, url_prefix='/api/products')
     app.register_blueprint(order_blueprint, url_prefix='/api/orders')
     app.register_blueprint(production_blueprint, url_prefix='/api/productions')
+    app.register_blueprint(user_blueprint, url_prefix='/api/users')
 
 
 def configure_rate_limit():
@@ -43,6 +46,7 @@ def configure_rate_limit():
     limiter.limit('13/minute')(product_blueprint)
     limiter.limit('7/minute')(order_blueprint)
     limiter.limit('7/hour')(production_blueprint)
+    limiter.limit('21/hour')(user_blueprint)
 
 
 if __name__ == '__main__':
@@ -52,7 +56,7 @@ if __name__ == '__main__':
     configure_rate_limit()
 
     with app.app_context():
-        # db.drop_all()
+        db.drop_all()
         db.create_all()
 
     app.run(debug=True)
