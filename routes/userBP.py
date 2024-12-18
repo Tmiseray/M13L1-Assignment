b699ca4369f4e15b3cc63ac9188469dcc43b4910
Tmiseray <161903165+Tmiseray@users.noreply.github.com>
2024-12-17 19:37:39 -0600

    Fixing typo

--- routes/userBP.py
+++ routes/userBP.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
-from controllers.userController import login
+from controllers.userController import save, find_all, login
 
 user_blueprint = Blueprint('userBP', __name__)
+user_blueprint.route('/', methods=['POST'])(save)
+user_blueprint.route('/', methods=['GET'])(find_all)
 user_blueprint.route('/login', methods=['POST'])(login)
